98649c034e81224ed9ec2ecec7ebcc07d46e16f8
Pieter Schoenmakers <pieter.schoenmakers@hypoport.com>
2021-10-21 13:22:09 +0200

    setup/synonyms_core_public.sql: New file.

---
+++ setup/synonyms_core_public.sql
@@ -0,0 +1,14 @@
+
+create public synonym ax_crypto for crypto_util_pkg;
+create public synonym ax_date for date_util_pkg;
+create public synonym ax_debug for debug_pkg;
+create public synonym ax_encode for encode_util_pkg;
+create public synonym ax_file for file_util_pkg;
+create public synonym ax_math for math_util_pkg;
+create public synonym ax_random for random_util_pkg;
+create public synonym ax_raw for raw_util_pkg;
+create public synonym ax_regexp for regexp_util_pkg;
+create public synonym ax_sql for sql_util_pkg;
+create public synonym ax_string for string_util_pkg;
+create public synonym ax_ooxml for ooxml_util_pkg;
+create public synonym ax_zip for zip_util_pkg;
